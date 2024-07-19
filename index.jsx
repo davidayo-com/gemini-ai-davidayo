4a0410a97890c06eda2c7b376e498ca0bba349d0
daviesayo <daviesayo14@gmail.com>
2024-07-19 13:15:18 -0500

    add start scripts

--- index.jsx
+++ index.jsx
@@ -1,16 +1,28 @@
-const { GoogleGenerativeAI } = require("@google/generative-ai");
-
-// Access your API key as an environment variable (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI(process.env.API_KEY);
-
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-async function run() {
-  const prompt = "Write a story about a AI and magic";
-
-  const result = await model.generateContent(prompt);
-  const response = await result.response;
-  const text = response.text();
-  console.log(text);
+if (process.env.NODE_ENV !== "production") {
+  require('dotenv').config();
 }
+// Load environment variables from .env file
+const express = require("express");
 
-run();
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Set the view engine to ejs
+app.set("view engine", "ejs");
+
+// Serve static files
+app.use(express.static("public"));
+
+// Define the route for the home page
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/artificial-intelligence/", (req, res) => {
+  res.render("artificial-intelligence");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
